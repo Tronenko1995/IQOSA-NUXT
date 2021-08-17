ee0dac1fd45b9ab20eb36a9a62064bca09c82b76
Igor Kondrashov <igorkondrashov@MacBook-Pro.local>
2021-08-17 17:19:06 +0300

    fast fix margins

--- components/projects/ProjectsGrid.vue
+++ components/projects/ProjectsGrid.vue
@@ -345,14 +345,15 @@ export default {
 }
 @media (max-width: 1440px) {
 .projects-grid {
-  margin-top: 255px;
+  margin-top: 230px;
   margin-bottom: 156px;
     &__title {
       font-size: 148px;
     }
   }
   .projects-interior {
-    margin-top: 156px;
+    // margin-top: 156px;
+    margin-top: 100px;
   }
 }
 @media (max-width: 1280px) {
